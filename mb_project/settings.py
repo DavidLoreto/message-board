90028d48eb590bfd62da5b46166ddb7b03076b1d
David Loreto <dloretosalas@gmail.com>
2022-09-30 12:34:56 +0000

    Updated files for heroku deplyoment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j7dq09(9tnceke#j*iyj2t4-!h!jn*2)g&t#h99amij_g&v&&(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
